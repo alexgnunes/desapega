7b7bba0815fbb4a26b7492d305a0e1e2dc686e8b
Alex Nunes <alexgaribalde_tb@hotmail.com>
2024-07-27 09:27:41 -0300

    criação do projeto desapega

---
+++ src/main/java/br/com/futurodev/desabega/DesabegaApplication.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.desabega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesabegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesabegaApplication.class, args);
+	}
+
+}
